3131302b29f1719de9dce29a72561506559cf105
knnaliev95 <knnaliev95@gmail.com>
2022-08-07 16:04:06 +0400

    Blueprint

---
+++ admin/routes.py
@@ -0,0 +1,38 @@
+from flask import render_template,redirect,request
+from admin import admin_bp
+from models import *
+
+@admin_bp.route('/add', methods=['GET','POST'])
+def product_add():
+    if request.method=='POST':
+        productName=request.form['productName']
+        productPrice=request.form['productPrice']
+        product=Product(productName=productName,productPrice=productPrice)
+        db.session.add(product)
+        db.session.commit()
+        return redirect('/list')
+    return render_template('add.html')
+
+@admin_bp.route('/list', methods=['GET','POST'])
+def product_list():
+    products=Product.query.all()
+    return render_template('list.html', products=products)
+
+@admin_bp.route('/update/<id>', methods=['GET','POST'])
+def product_update(id):
+    product=Product.query.get(id)
+    if request.method=='POST':
+        p_name=request.form['p_name']
+        p_price=request.form['p_price']
+        product.productName=p_name
+        product.productPrice=p_price
+        db.session.commit()
+        return redirect('/list')
+    return render_template('update.html', product=product)
+
+@admin_bp.route('/delete/<id>', methods=['GET','POST'])
+def product_delete(id):
+    product=Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect('/list')
--- start.py
+++ start.py
@@ -1,13 +1,14 @@
-from flask import Flask,request,redirect,url_for,flash,render_template
+from flask import Flask,render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 db=SQLAlchemy(app)
-migrate=Migrate(app,db)
-from routes.admin.product import *
 
-if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+from admin.routes import *
+
+from admin import admin_bp
+
+app.register_blueprint(admin_bp)
+
+if __name__=='__main__':
+    app.run(port=8000,debug=True)
